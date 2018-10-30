a8d81c76396577ffb49550262e6473905d8b4531
Bruno Borges <bruno.borges@microsoft.com>
2018-10-29 17:21:26 -0700

    simplified local repo

--- src/main/java/com/microsoft/springframework/samples/dao/TodoItemRepositoryLocal.java
+++ src/main/java/com/microsoft/springframework/samples/dao/TodoItemRepositoryLocal.java
@@ -20,32 +20,13 @@ import java.util.stream.StreamSupport;
 @Primary
 @ConditionalOnMissingBean(name = "cosmosdb")
 @Repository("local")
-public class TodoItemRepositoryLocal implements PagingAndSortingRepository<TodoItem, String>, ITodoItemRepository {
+public class TodoItemRepositoryLocal implements ITodoItemRepository {
 
     private Map<String, TodoItem> data = new HashMap<>();
 
     @Override
-    public Iterable<TodoItem> findAll(Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<TodoItem> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
     public TodoItem save(TodoItem entity) {
         return data.put(entity.getID(), entity);
-    }
-
-    @Override
-    public <S extends TodoItem> Iterable<S> saveAll(Iterable<S> entities) {
-        for (S s : entities) {
-            data.put(s.getID(), s);
-        }
-
-        return entities;
     }
 
     @Override
@@ -54,23 +35,8 @@ public class TodoItemRepositoryLocal implements PagingAndSortingRepository<TodoI
     }
 
     @Override
-    public boolean existsById(String s) {
-        return data.containsKey(s);
-    }
-
-    @Override
     public Iterable<TodoItem> findAll() {
         return StreamSupport.stream(data.values().spliterator(), false).collect(Collectors.toList());
-    }
-
-    @Override
-    public Iterable<TodoItem> findAllById(Iterable<String> strings) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return data.size();
     }
 
     @Override
@@ -78,18 +44,4 @@ public class TodoItemRepositoryLocal implements PagingAndSortingRepository<TodoI
         data.remove(s);
     }
 
-    @Override
-    public void delete(TodoItem entity) {
-        data.values().remove(entity);
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends TodoItem> entities) {
-        data.values().removeAll(StreamSupport.stream(entities.spliterator(), false).collect(Collectors.toList()));
-    }
-
-    @Override
-    public void deleteAll() {
-        data.clear();
-    }
 }
